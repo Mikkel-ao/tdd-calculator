b4b7345221a5b004855a1144dd8b7856dd34c577
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-02-02 12:28:02 +0100

    Setting up project. Adding project to repo

---
+++ src/main/java/org/cphbusiness/Main.java
@@ -0,0 +1,7 @@
+package org.cphbusiness;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/org/cphbusiness/tdd/CalculatorTest.java
@@ -0,0 +1,50 @@
+package org.cphbusiness.tdd;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+    Calculator instance = new Calculator();
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testAdd() {
+        // Calculator calculator = new Calculator();
+        int expected = 0;
+        int actual = instance.add(-2, 2);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void testSubtract() {
+        int expected = 0;
+        int actual = instance.subtract(-2,-2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testMultiply() {
+        int expected = 4;
+        int actual = instance.multiply(2, 2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testDivide() {
+        double expected = 2;
+        double actual = instance.divide(8, 4);
+        assertEquals(expected, actual);
+    }
+}
